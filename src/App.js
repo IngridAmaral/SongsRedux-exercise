ff2c42e5e38d1ac8c2f22fe9e851e0957ea1dba4
Ingrid Amaral <guid_95@hotmail.com>
2020-02-19 14:57:06 +0100

    song details

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import SongDetail from './components/SongDetail';
 class App extends React.Component {
     render () {
         return (
-            <div>
-                <SongList />
+            <div className='ui container grid'>
+                <div className='ui row'>
+                    <div className='column eight wide'>
+                        <SongList />
+                    </div>
+                    <div className='column four wide'>
+                        <SongDetail />
+                    </div>
+                </div> 
             </div>
         )
     }
--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
 class SongDetail extends React.Component {
     render () {
+        const { song } = this.props
+        if (!song) {
+            return <div>Select a song</div>
+        }  
         return (
-            <div>Song Detail</div>
+            <div className='item'>
+                    <div className='left floated content'>
+                        <div className='content'>
+                            {song.title}
+                        </div>
+                        <div className='content'>
+                            {song.duration}
+                        </div>
+                    </div>
+            </div>
         )
     }
 }
 
-export default SongDetail
+                
+
+const mapSateToProps  = state => {
+    console.log(state)
+    return { song: state.selectedSong }
+}
+
+export default connect(mapSateToProps)(SongDetail);
