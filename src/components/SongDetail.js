1b7dea7155292db0a982e4201c5f5d08a2f3b582
Ingrid Amaral <guid_95@hotmail.com>
2020-02-19 15:07:21 +0100

    final version

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -5,14 +5,16 @@ class SongDetail extends React.Component {
     render () {
         const { song } = this.props
         if (!song) {
-            return <div>Select a song</div>
+            return <div><h3>Select a song</h3></div>
         }  
         return (
             <div className='item'>
                     <div className='left floated content'>
+                        <h3>Details for:</h3>
                         <div className='content'>
                             {song.title}
                         </div>
+                        <br />
                         <div className='content'>
                             {song.duration}
                         </div>
